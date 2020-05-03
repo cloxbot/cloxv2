21fa19a29f2a733c97e0e04db2c4219fd047dc3e
cloxbot <64756295+cloxbot@users.noreply.github.com>
2020-05-04 01:01:49 +0200

    Create bird.js

---
+++ commands/animals/bird.js
@@ -0,0 +1,25 @@
+      const { RichEmbed } = require("discord.js");
+const Discord = require("discord.js");
+const randomPuppy = require("random-puppy");
+const fetch = require('node-fetch');
+const snekfetch = require('snekfetch')
+
+module.exports = {
+  name: "bird",
+  aliases: [""],
+  category: "animals",
+  description: "This command gives you a random birds photos",
+  usage: "[command | alias]",
+  run: async (client, message, args) => {
+          const superagent = require("superagent");
+      const { body } = await superagent
+      .get('https://some-random-api.ml/img/birb');
+      const embed  = new Discord.RichEmbed()
+      .setTitle("Clox Bot birds")
+      .setColor("#51B322")
+      .setImage(body.link)
+      
+      message.channel.send({embed})
+}
+
+  }
